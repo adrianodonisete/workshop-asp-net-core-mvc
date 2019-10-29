4ae36c8f570382e601d03705d590eb26623459c6
Adriano Machado <adrianodonisete@gmail.com>
2019-10-29 18:34:10 -0300

    config mysql

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 
 namespace SalesWebMvc
 {
@@ -28,7 +29,10 @@ namespace SalesWebMvc
             services.AddControllersWithViews();
 
             services.AddDbContext<SalesWebMvcContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
+                        builder.MigrationsAssembly("SalesWebMvc")));
+
+            //options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), ));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
